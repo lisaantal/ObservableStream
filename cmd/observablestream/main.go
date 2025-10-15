5ce1f95b8cdf0207d3c02a689ba2b28d8238ce33
Lisa <78325361+lisaantal@users.noreply.github.com>
2025-10-15 20:38:07 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/observablestream/main.go
@@ -0,0 +1,23 @@
+// cmd/observablestream/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "observablestream/internal/observablestream"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := observablestream.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
